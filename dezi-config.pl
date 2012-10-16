823a0440f8e36688b5a02258ccba7fe404896f27
Peter Karman <pkarman@mpr.org>
2012-10-16 09:31:35 -0500

    add stemmer to match indexer config

--- dezi-config.pl
+++ dezi-config.pl
@@ -1,10 +1,23 @@
 # Dezi server config for use with Wordpress plugin dezi-for-wordpress
+
+# english stemmer by default -- do not need this if FuzzyIndexingMode
+# is not set below
+use Lingua::Stem::Snowball;
+my $snowball = Lingua::Stem::Snowball->new(
+    lang     => 'en',
+    encoding => 'UTF-8',
+);
+my $stemmer = sub {
+    my ( $qp, $term ) = @_;
+    return $snowball->stem($term);
+};
+
 {
 
     engine_config => {
         default_response_format => 'JSON',
 
-        # could be any Search::OpenSearch::Engine::* class
+        # the Search::OpenSearch::Engine::* class
         type => 'Lucy',
 
         # name of the index(es)
@@ -12,11 +25,12 @@
 
         # which facets to calculate, and how many results to consider
         facets => {
-            names       => [qw( categories tags author type  )],  # TODO derive f= param
+            names => [qw( categories tags author type  )]
+            ,    # TODO derive f= param
             sample_size => 10_000,
         },
-  
-        # how many seconds should facets be cached? short in dev, set to longer for better performance.
+
+# how many seconds should facets be cached? short in dev, set to longer for better performance.
         cache_ttl => 30,
 
         # result attributes in response
@@ -50,6 +64,10 @@
 
             # store token positions to optimize snippet creation
             highlightable_fields => 1,
+        },
+        parser_config => {
+            query_dialect => 'Lucy',
+            stemmer       => $stemmer,
         },
     },
 
